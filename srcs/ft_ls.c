8a57089f187d0e658cdb7d26f2673c0b9f72be03
Piriou ENZO <penzo@e2r9p17.42.fr>
2019-01-10 16:02:06 +0100

    added penzo's ft_ls.c

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -6,17 +6,43 @@
 /*   By: pscott <pscott@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 18:51:45 by pscott            #+#    #+#             */
-/*   Updated: 2019/01/10 15:20:04 by pscott           ###   ########.fr       */
+/*   Updated: 2019/01/10 15:58:12 by penzo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+void	ft_ls(const char *path, t_opt *opt)
+{
+	struct dirent	*dirent;
+	DIR				*directory;
+	t_ldir			*ldir;
+
+	if(!(directory = opendir(path)))//TODO: protect
+		//error
+	while (dirent = readdir)
+	{
+		add_ldir(ldir, dirent, path, opt);
+	}
+	print_ldir(ldir, opt);
+	while (ldir)
+	{
+		if (ldir->d_type == 8)
+		{
+			printf("%s:\n", ldir->dirname);
+			ft_ls(ldir->path, opt);
+		}
+		ldir = ldir->next;
+	}
+	//free list
+	return;//TODO: return value
+}
+
 int	main(int argc, char **argv)
 {
 	t_opt *opt;
 
-//	malloc everything ?
+	//	malloc everything ?
 	if (argv)
 		(*argv)++;
 	opt = malloc_opt();

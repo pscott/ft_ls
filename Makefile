7f43ca62079eb69f72961a0e41358a56ddd86f43
Piriou SCOTT <pscott@student.42.fr>
2019-01-17 15:24:46 +0100

    Implemented sorting of argv, fixed extra n, and fixed SEGV on empty dir

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: pscott <pscott@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/01/15 13:54:38 by pscott            #+#    #+#              #
-#    Updated: 2019/01/16 23:05:15 by pscott           ###   ########.fr        #
+#    Updated: 2019/01/17 15:21:37 by pscott           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -28,7 +28,7 @@ OBJS	:= $(LS:.c=.o)
 DEPS	:= Makefile includes/ft_ls.h
 
 COMP	:= $(CC) $(WFLAGS) $(INCL) $(LIBS)
-OPT		:= .
+OPT		:= -R includes/empty
 
 all: makelib $(NAME)
 
@@ -36,8 +36,8 @@ makelib:
 	@make -C libft
 
 diff: all
-	@./$(NAME) $(OPT) > a
-	@ls -C $(OPT) > b
+	@./$(NAME) $(OPT) 1> a
+	@ls -C $(OPT) 1> b
 	@diff a b
 	@$(RM) a
 	@$(RM) b
--- srcs/open_once.c
+++ srcs/open_once.c
@@ -6,30 +6,75 @@
 /*   By: pscott <pscott@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 18:43:19 by pscott            #+#    #+#             */
-/*   Updated: 2019/01/14 19:23:42 by pscott           ###   ########.fr       */
+/*   Updated: 2019/01/17 14:46:38 by pscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int			open_once(int argc, char **argv)
+void	rearrange_argv(int i, int argc, char **argv)
+{
+	int j;
+	j = 0;
+	/*while (j < argc)
+	  {
+	  printf("ARGV: %s\n", argv[j]);
+	  j++;
+	  }*/
+	while (i < argc)
+	{
+		argv[i] = argv[i + 1];
+		i++;
+	}
+	argv[i] = NULL;
+}
+
+void	sort_argv(int argc, char **argv)
+{
+	char	*tmp;
+	int		i;
+	int		j;
+
+	j = 0;
+	while (j < argc - 1)
+	{
+		i = 0;
+		while (i < argc- 1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				tmp = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = tmp;
+			}
+			i++;
+		}
+		j++;
+	}
+}
+
+void		open_once(int *argc, char **argv, int *ret)
 {
 	DIR		*d;
 	int		i;
 	int		modif;
 
-	i = 0;
 	modif = 0;
-	while (i < argc)
+	sort_argv(*argc, argv);
+	i = 0;
+	while (argv[i])
 	{
 		if (!(d = opendir(argv[i])))
 		{
 			error_open(argv[i]);
 			modif++;
+			rearrange_argv(i, *argc, argv);
+			(*argc)--;
+			*ret = 1;
 		}
+		else
+			i++;
 		if (d)
 			closedir(d);
-		i++;
 	}
-	return (modif);
 }

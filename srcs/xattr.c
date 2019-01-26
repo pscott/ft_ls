c53cc95016d11e8f2880c47bc407a01c1dd7e582
Piriou ENZO <penzo@student.42.fr>
2019-01-26 16:08:00 +0100

    added acl: '+'

--- srcs/xattr.c
+++ srcs/xattr.c
@@ -6,21 +6,31 @@
 /*   By: penzo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 17:40:24 by penzo             #+#    #+#             */
-/*   Updated: 2019/01/18 21:23:46 by penzo            ###   ########.fr       */
+/*   Updated: 2019/01/26 16:00:42 by penzo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+#include <sys/acl.h>
 
 char	get_attr_char(char *path)
 {
-	char	c;
-	ssize_t xattr;
+	char		c;
+	ssize_t 	xattr;
+	acl_t		acl;
+	acl_entry_t dummy;
 
+	acl = acl_get_link_np(path, ACL_TYPE_EXTENDED);
+	if (acl && acl_get_entry(acl, ACL_FIRST_ENTRY, &dummy) == -1) {
+		acl_free(acl);
+		acl = NULL;
+	}
 	xattr = listxattr(path, NULL, 0, XATTR_NOFOLLOW);
 	c = '\0';
 	if (xattr > 0)
 		c = '@';
+	else if (acl != NULL)
+		c = '+';
 	else
 		c = ' ';
 	return (c);
